79221e7a98ebc664fa20ffed0504a38d565aa846
MTY <tianyuan.mao@zebra-c.com>
2020-03-17 22:42:20 +0800

    feat: add prompts

---
+++ prompts.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: '是否将仓库设置为私有?',
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: '请输入作者?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: '请输入项目描述信息?',
+  },
+]
